1a6902b7cb1a8f3107fea65b10cd2bce3f60b406
Alstocras <alstoperiment@gmail.com>
2026-01-12 17:08:39 +0000

    demo for one pixel's redshift correction its so cool WOWOW

--- simulations/wavelength-test.py
+++ simulations/wavelength-test.py
@@ -1,5 +1,12 @@
 import wlrgb
+import math
 
+distance_galaxy = float(input("Distance to galaxy in m(use scientific e notation):"))
+observed_wavelength_galaxy = float(input("Observed wavelength in nm:"))
 
-rgb_val = wlrgb.wavelength_to_rgb(627.7)
-print(rgb_val)
+velocity_galaxy = distance_galaxy * 2.23e-18
+redshift_galaxy = (1 + (velocity_galaxy / 3e8)) * (1 / (math.sqrt(1 - (velocity_galaxy ** 2) / ((3e8) ** 2)))) - 1
+emitted_wavelength_galaxy = (observed_wavelength_galaxy) / (redshift_galaxy + 1)
+rgb_val_galaxy = wlrgb.wavelength_to_rgb(emitted_wavelength_galaxy)
+
+print(f"The RGB value of the pixel in question should be {rgb_val_galaxy}")
