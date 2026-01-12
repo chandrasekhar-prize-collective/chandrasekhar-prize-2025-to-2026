fb07570d24eeb80c811e33c5017f2eb1b0828059
Alstocras <alstoperiment@gmail.com>
2026-01-12 14:36:38 +0000

    added python things YAY protoype YAY wowow

--- simulations/example.java
+++
@@ -1,7 +0,0 @@
-package simulations;
-
-public class example{
-    public static void main(String[] args) {
-        System.out.println("528491");
-    }
-}
---
+++ simulations/wavelength-test.py
@@ -0,0 +1,5 @@
+import wlrgb
+
+
+rgb_val = wlrgb.wavelength_to_rgb(380)
+print(rgb_val)
